eb7530069fe9fbfc5c15dda8bdc76f2bcb912b63
Qichen Zhu <zqc9@hotmail.com>
2022-12-14 01:29:21 -0500

    1214

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,56 @@
+import { useState, useEffect } from 'react';
+import Product from '../components/Product';
+
 export default function Home() {
+  const [productsInfo, setProductsInfo] = useState([]);
+  const [phrase, setPhrase] = useState('');
+
+  useEffect(() => {
+    fetch('/api/products')
+      .then((response) => response.json())
+      .then((json) => setProductsInfo(json));
+  }, [])
+
+  const categoriesNames = [...new Set(productsInfo.map(p => p.category))];
+
+  let products;
+  if (phrase) {
+    products = productsInfo.filter(p => p.name.toLowerCase().includes(phrase));
+  } else {
+    products = productsInfo;
+  }
+
   return (
     <div className="p-5">
+      <input value={phrase}
+        onChange={e => setPhrase(e.target.value)}
+        type="text"
+        placeholder="Search for products..."
+        className="bg-gray-100 w-full py-2 px-4 rounded-xl" />
       <div>
-        <h2 className="text-2xl">Mobiles</h2>
-        <div className="py-4">
-          <div className="w-64">
-            <div className="bg-blue-100 p-5 rounded-xl">
-              <img src="/products/iphone.png" alt="" />
-            </div>
-            <div className="mt-2">
-              <h3 className="font-bold text-lg">Iphone 14 Pro</h3>
-            </div>
-            <p className="text-sm mt-1 leading-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
-            <div className="flex mt-1">
-              <div className="text-2xl font-bold grow">$899</div>
-              <button className="bg-emerald-400 text-white py-1 px-3 rounded-xl">+</button>
-            </div>
+        {categoriesNames.map(categoryName => (
+          <div key={categoryName}>
+            {products.find(p => p.category === categoryName) && (
+              <div>
+                <h2 className="text-2xl py-5 capitalize">
+                  {categoryName}
+                </h2>
+                <div className='flex -mx-5 overflow-x-scroll snap-start scrollbar-hide'>
+                  {products.filter(
+                    p => p.category === categoryName).map(
+                      productInfo => (
+                        <div key={productInfo._id} className="px-5 snap-start">
+                          <Product {...productInfo} />
+                        </div>
+                      ))}
+                </div>
+              </div>
+            )}
           </div>
+        ))}
+        <div className="py-4">
         </div>
       </div>
-    </div>
+    </div >
   )
 }
